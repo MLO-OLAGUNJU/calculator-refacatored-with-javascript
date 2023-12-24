e3fae661cb4638c50f8d3dd5633c0a892498464c
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2023-12-24 16:42:45 +0100

    added some javascript constant variables

---
+++ script.js
@@ -0,0 +1,11 @@
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelectorAll("[data-equals]");
+const deleteButton = document.querySelectorAll("[data-delete]");
+const allClearButton = document.querySelectorAll("[data-all-clear]");
+const previousOperandsandTextElement = document.querySelectorAll(
+  "[data-previous-operand]"
+);
+const currentOperandsandTextElement = document.querySelectorAll(
+  "[data-current-operand]"
+);
